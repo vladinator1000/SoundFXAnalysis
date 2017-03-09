cd6ff3cd49ca1af9da7fe5023011aae7cb9c551e
Vlady Veselinov <vvesel200@lab-m329-mac-12.enterprise.gcal.ac.uk>
2017-03-09 16:23:05 +0000

    Iterate through JSON

--- src/statistics.py
+++ src/statistics.py
@@ -5,8 +5,16 @@ import scipy.stats
 import json
 from pprint import pprint
 
-# with open('maxPatchRecording_analysis.json') as data_file:
-#     data = json.load(data_file)
-#
-# pprint(data['sfx']['inharmonicity'])
+with open('maxPatchRecording_aggregated_analysis.json') as data_file:
+    data = json.load(data_file)
+
+
 # dateTimeNow = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
+# Iterate through object
+for key in data:
+    print key + ": "
+
+    for keyDeeper in data[key]:
+        print keyDeeper
+    print
