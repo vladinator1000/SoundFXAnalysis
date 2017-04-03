f1f6ac99b47960e60fabbc57216c4a82092d6d03
Vlady Veselinov <vladkodmc@gmail.com>
2017-04-03 23:53:13 +0100

    Read analysis files and prepare for statistics

--- src/readJson.py
+++ src/readJson.py
@@ -1,7 +1,60 @@
-import json
+import json, os
 from pprint import pprint
 
-with open('A_ball_bounce_arcady_1_analysis.json') as openedJson:
-	data = json.load(openedJson)
 
-pprint(data["sfx"])
+fileNames = []
+filePaths = []
+aggregateFileNames = []
+aggregateFilePaths = []
+
+# Get the names and paths of all files in the results folder
+resultsDirectoryPath = os.path.dirname(os.path.realpath(__file__)) + '/../analysisResults/'
+
+for file in os.listdir(resultsDirectoryPath):
+	if file.endswith('.json'):
+		if 'aggregate' in file:
+			aggregateFileNames.append(file)
+			aggregateFilePaths.append(os.path.join(resultsDirectoryPath, file))
+		else:
+			fileNames.append(file)
+			filePaths.append(os.path.join(resultsDirectoryPath, file))
+
+
+dataSets = []
+aggregatedDataSets = []
+
+for path in filePaths:
+	with open(path) as openedJson:
+		dataSets.append(json.load(openedJson))
+
+for path in aggregatedFilePaths:
+	with open(path) as openedJson:
+		aggregatedDataSets.append(json.load(openedJson))
+
+
+# Flatten an dictionary recursively (retun only furthest nodes)
+# like this: http://i.imgur.com/8XpvXMw.jpg
+def flatten(dictionary, accumulator = [], excludeKeys = []):
+	for key, value in dictionary.iteritems():
+		if isinstance(value, dict):
+			flatten(value, accumulator, excludeKeys)
+		elif key not in excludeKeys:
+			accumulator.append({ key: value })
+			# print '{0} : {1}'.format(key, value)
+
+	return accumulator;
+
+
+# Flatten the tree and load it into an array for all data sets
+flatDataSets = []
+for set in dataSets:
+	flatDataSets.append([flatten(set, excludeKeys = ['essentia'])])
+
+flatAggregatedDataSets = []
+for aggregatedSet in aggregatedDataSets:
+	flatAggregatedDataSets.append([flatten(aggregatedSet, excludeKeys = ['essentia'])])
+
+
+for set in flatDataSets:
+	for key in set:
+		pprint(key)
