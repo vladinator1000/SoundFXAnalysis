7bb099a326d67f56927103f422d8817f8a90790c
Vlady Veselinov <vladkodmc@gmail.com>
2017-04-04 03:17:21 +0100

    Get euclidian distance and write resulting set to JSON

--- src/analyseJSON.py
+++ src/analyseJSON.py
@@ -42,6 +42,8 @@ aggregateDataSetsA = []
 dataSetsB = []
 aggregateDataSetsB = []
 
+
+# Load data from JSON files
 for path in filePathsA:
 	with open(path) as openedJson:
 		dataSetsA.append(json.load(openedJson))
@@ -61,27 +63,52 @@ for path in aggregateFilePathsB:
 
 
 # Only use SFX descriptors
-sfxDescriptorSetsA = []
-sfxDescriptorSetsB = []
-results = []
+sfxAggregateDescriptorSetsA = []
+sfxAggregateDescriptorSetsB = []
+euclidianDistanceSets = []
 
-for dict in dataSetsA:
-	sfxDescriptorSetsA.append(dict['sfx'])
+for set in aggregateDataSetsA:
+	sfxAggregateDescriptorSetsA.append(set['sfx'])
 
-for dict in dataSetsB:
-	sfxDescriptorSetsB.append(dict['sfx'])
-
-for index, dict in enumerate(sfxDescriptorSetsA):
-	for (tupleA, tupleB) in zip(sfxDescriptorSetsA[index].iteritems(), sfxDescriptorSetsB[index].iteritems()):
-		print tupleA, tupleB
+for set in aggregateDataSetsB:
+	sfxAggregateDescriptorSetsB.append(set['sfx'])
 
 
+# Calculate euclidian distance for each value per pair
+# https://en.wikipedia.org/wiki/Euclidean_distance
+# http://stackoverflow.com/questions/1401712/how-can-the-euclidean-distance-be-calculated-with-numpy
+for index, set in enumerate(sfxAggregateDescriptorSetsA):
+	distanceSet = dict()
+	for (tupleA, tupleB) in zip(sfxAggregateDescriptorSetsA[index].iteritems(), sfxAggregateDescriptorSetsB[index].iteritems()):
+		propertyName = tupleA[0]
+
+		if type(tupleA[1]) is dict:
+			print propertyName + ' Euclidian distances:'
+
+			# Create nested dictionary for mean, max, min, etc...
+			distanceSet[propertyName] = dict()
+			for key in tupleA[1]:
+				distance = numpy.linalg.norm(numpy.array(tupleA[1][key]) - numpy.array(tupleB[1][key]))
+				distanceSet[propertyName][key] = distance
+				print key, distance
+
+		else:
+			distanceSet[propertyName] = distance
+			print propertyName, numpy.linalg.norm(tupleA[1] - tupleB[1])
+
+		print '\n'
+
+	euclidianDistanceSets.append(distanceSet)
 
 
+# Write distances to json in the ../analysisResults folder
+with open(resultsDirectoryPath + 'euclidianDistanceSets.json', 'w') as outFile:
+	json.dump(euclidianDistanceSets, outFile, sort_keys = True, indent = 4)
 
-
-# Flatten an dictionary recursively (retun only furthest nodes)
+# Flatten an dictionary recursively (retun only furthest nodes),
 # like this: http://i.imgur.com/8XpvXMw.jpg
+# doesn't work on aggregate sets, found that the hard way :(
+
 def flatten(dictionary, accumulator = [], excludeKeys = []):
 	for key, value in dictionary.iteritems():
 		if isinstance(value, dict):
